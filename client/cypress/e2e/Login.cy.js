b6c6e1361b95e0ce716f69f1c38e430fc854dc68
Scott Imandt <scottimandt@gmail.com>
2023-04-03 12:05:04 -0400

    Added Cypress Test files and updated package.json

---
+++ client/cypress/e2e/Login.cy.js
@@ -0,0 +1,23 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://playlister-hw4-vercel.vercel.app/')
+
+
+    cy.get('.css-11iqw2i > .MuiButtonBase-root').click();
+    
+    cy.get('[tabindex="0"] > a').click();
+
+    cy.url().should('include', '/login');
+
+    cy.get('#email').type('email')
+    cy.get('#email').should('have.value', 'email')
+
+    cy.get('#password').type('12345678')
+    cy.get('#password').should('have.value', '12345678')
+
+    cy.get('.css-1y8ugea > .MuiBox-root > .MuiButtonBase-root').click();
+
+    cy.get('#list-selector-list')
+
+  })
+})
