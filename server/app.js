4a50c481aec930aedc207623d141d3911f1c2f10
ZKhan <Zaiff.khan@stonybrook.edu>
2023-04-03 14:31:36 -0400

    testing port change

---
+++ server/app.js
@@ -0,0 +1,31 @@
+// THESE ARE NODE APIs WE WISH TO USE
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+// CREATE OUR SERVER
+dotenv.config()
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// SETUP THE MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: ['https://playlister-hw4-vercel.vercel.app'],
+    credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+// SETUP OUR OWN ROUTERS AS MIDDLEWARE
+const authRouter = require('./routes/auth-router')
+app.use('/auth', authRouter)
+const playlistsRouter = require('./routes/playlists-router')
+app.use('/api', playlistsRouter)
+
+// INITIALIZE OUR DATABASE OBJECT
+const db = require('./db')
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
+const PORT = process.env.PORT || 4000;
+
 // PUT THE SERVER IN LISTENING MODE
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
